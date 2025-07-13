124aec9ffb8fbe40202aa6407194af25621033cb
venkatesh <venkatesh8330b@gmail.com>
2025-07-13 10:43:57 +0530

    Update app.py

--- flask-app/flask-app/app.py
+++ flask-app/flask-app/app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     html = """
+ <!DOCTYPE html>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
